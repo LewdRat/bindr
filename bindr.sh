727690b21d46262dbe0ec4bde2cb326f3c23c1bc
LewdRat <kennymccormic68@gmail.com>
2025-03-27 14:23:57 +0300

    bindr.sh

--- bindr.sh
+++ bindr.sh
@@ -5,7 +5,7 @@ systemctl enable --now bind
 mkdir /etc/bind/zone/master
 
 cp /bindr/au-team.irpo /etc/bind/zone/master/au-team.irpo
-cp /bindr/10.168.192.in-addr.arpa
+cp /bindr/10.168.192.in-addr.arpa /etc/bind/zone/master/10.168.192.in-addr.arpa
 
 chown root:named /etc/bind/zone/master
 chown root:named /etc/bind/zone/master/*
